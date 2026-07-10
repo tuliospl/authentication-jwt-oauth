027c0a409a1e7e618db2d36aaa0563985f19c050
Tulio.Sploradori <tulio.sploradori@ab-inbev.com>
2026-07-10 19:02:58 -0300

    first commit

---
+++ src/test/java/com/authentication/jwt_oauth/JwtOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authentication.jwt_oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
